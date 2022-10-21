a80137aa58b6f341f6bb3f09f7f4f5cc99cc45d6
Ellen Kecia <ellenkeciaas@gmail.com>
2022-10-21 07:47:18 -0300

    inicio alteração card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../pages/Home.css';
 
 class ProductCard extends React.Component {
   render() {
